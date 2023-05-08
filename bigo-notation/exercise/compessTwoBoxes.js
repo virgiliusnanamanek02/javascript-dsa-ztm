dc5dfa59e2208a79bf622bb06f99d023b1e29c64
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-05-08 17:24:38 +0700

    Exercise: O(a+b) time complexity

---
+++ bigo-notation/exercise/compessTwoBoxes.js
@@ -0,0 +1,6 @@
+function compessTwoBoxes(boxes1, boxes2) {
+  boxes1.forEach(box => console.log(box))
+  boxes2.forEach(box => console.log(box))
+}
+
+// The Big O Notation for this function is O(a + b) --> Because we have two different inputs
