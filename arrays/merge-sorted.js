daeeb26de973d9ec1ffacada82cb04d1fd94ff7b
vickynana19 <vickynana034@gmail.com>
2023-09-14 22:40:43 +0700

    Optimized merge-sorted runtime

--- arrays/merge-sorted.js
+++ arrays/merge-sorted.js
@@ -1,29 +1,56 @@
-function mergeSortedArr(firstArr, secArr) {
-  let lengthArr = 0;
-  let firstIdxArr = 0;
-  let secIdxArr = 0;
-  let result = [];
+const mergeSortedArr = function (nums1, m, nums2, n) {
+  let i = m - 1; // Last index of nums1
+  let j = n - 1; // Indeks terakhir dari nums2
+  let k = m + n - 1; // Indeks terakhir dari nums1 setelah penggabungan
 
-  const totalLengthArr = firstArr.length + secArr.length;
-
-  while (lengthArr < totalLengthArr) {
-    let isFirstArrDepleted = firstIdxArr >= firstArr.length;
-    let isSecArrDepleted = secIdxArr >= secArr.length;
-    if (
-      !isFirstArrDepleted &&
-      (isSecArrDepleted || firstArr[firstIdxArr] < secArr[secIdxArr])
-    ) {
-      result[lengthArr] = firstArr[firstIdxArr];
-      firstIdxArr++;
+  // Mulai dari akhir dan geser ke depan
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
     } else {
-      result[lengthArr] = secArr[secIdxArr];
-      secIdxArr++;
+      nums1[k] = nums2[j];
+      j--;
     }
-
-    lengthArr++;
+    k--;
   }
 
-  return result;
-}
+  // Jika elemen-elemen di nums2 masih tersisa, tambahkan mereka ke nums1
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
 
-console.log(mergeSortedArr([1, 3, 5, 7], [2, 4, 6]));
+  return nums1;
+};
+
+console.log(mergeSortedArr([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(mergeSortedArr([1], 1, [], 0));
+
+const mergeSortedArrOpt = function (nums1, m, nums2, n) {
+  let i = m - 1; // Indeks terakhir dari nums1
+  let j = n - 1; // Indeks terakhir dari nums2
+  let k = m + n - 1; // Indeks terakhir dari nums1 setelah penggabungan
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
+  }
+
+  // Tidak perlu menangani sisa elemen di nums1, karena mereka sudah berada di tempat yang benar.
+
+  // Jika elemen-elemen di nums2 masih tersisa, tambahkan mereka ke nums1 (jika diperlukan)
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  }
+
+  // Kembalikan nums1 yang telah digabungkan (jika diperlukan)
+  return nums1;
+};
+
+console.log(mergeSortedArrOpt([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(mergeSortedArrOpt([1], 1, [1, 2, 0], 2));
