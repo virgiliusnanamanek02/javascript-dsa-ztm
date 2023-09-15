ff6d622b2cd995937673cc352b786143b979acfc
vickynana19 <vickynana034@gmail.com>
2023-09-15 18:40:56 +0700

    Exercise: hash table concepts

---
+++ hash-table/hash-table.js
@@ -0,0 +1,43 @@
+class HashTable {
+  constructor() {
+    this.values = {};
+    this.length = 0;
+    this.size = 0;
+  }
+
+  calculateHash(key) {
+    return key.toString().length % this.size;
+  }
+
+  add(key, value) {
+    const hash = this.calculateHash(key);
+    if (!this.values.hasOwnProperty(hash)) {
+      this.values[hash] = {};
+    }
+    if (!this.values.hasOwnProperty(key)) {
+      this.length++;
+    }
+
+    this.values[hash][key] = value;
+  }
+
+  search(key) {
+    const hash = this.calculateHash(key);
+    if (
+      this.values.hasOwnProperty(hash) &&
+      this.values[hash].hasOwnProperty(key)
+    ) {
+      return this.values[hash][key];
+    } else {
+      return null;
+    }
+  }
+}
+
+const ht = new HashTable();
+
+ht.add("Canada", "300");
+ht.add("Germany", "100");
+ht.add("Italy", "50");
+
+console.log(ht.search("Italy"));
