33e266d5946e339dfdc9d2b7731ac57b475cd899
vickynana19 <vickynana034@gmail.com>
2023-09-16 20:32:57 +0700

    Array: refactor find missing number

--- arrays/find-missing.js
+++ arrays/find-missing.js
@@ -1,6 +1,6 @@
 function findMissingNumber(nums) {
   const n = nums.length;
-  let total = ((n + 2) * (n + 1)) / 2;
+  let total = ((n + 1) / 2) * n;
 
   for (let i = 0; i < n; i++) {
     total -= nums[i];
@@ -9,4 +9,5 @@ function findMissingNumber(nums) {
   return total;
 }
 
-console.log(findMissingNumber([1, 2, 5, 6, 3, 7, 8]));
+console.log(findMissingNumber([0, 1, 2, 5, 6, 3, 7, 8]));
+console.log(findMissingNumber([0, 1]));
