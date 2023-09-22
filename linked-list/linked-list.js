b89f2fdde90e45eb1b3d239c6ccdd148da4c666b
vickynana19 <vickynana034@gmail.com>
2023-09-22 17:04:32 +0700

    Linkedlist: reversed linkedlist

---
+++ linked-list/linked-list.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.head = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  reverseLinkedList() {
+    let prev = null;
+    let current = this.head;
+    while (current) {
+      const nextTemp = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextTemp;
+    }
+
+    this.head = prev;
+  }
+  display() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+
+console.log("Before reversed:");
+linkedList.display();
+linkedList.reverseLinkedList();
+
+console.log("After reversed:");
+linkedList.display();
