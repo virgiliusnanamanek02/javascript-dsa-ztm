60441b22ab6483c3f1ca350b35ab579d50916ce6
vickynana19 <vickynana034@gmail.com>
2023-09-25 23:56:07 +0700

    Array: hourglassSum

---
+++ arrays/hourglass-sum.js
@@ -0,0 +1,29 @@
+const hourglassSum = (arr) => {
+  let sum = -63;
+  let max = 0;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      sum =
+        arr[i][j] +
+        arr[i][j + 1] +
+        arr[i][j + 2] +
+        arr[i + 1][j + 1] +
+        arr[i + 2][j] +
+        arr[i + 2][j + 1] +
+        arr[i + 2][j + 2];
+      max = max < sum ? sum : max;
+    }
+  }
+  return max;
+};
+
+const matrix = [
+  [1, 1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0],
+  [0, 0, 2, 4, 4, 0],
+  [0, 0, 0, 2, 0, 0],
+  [0, 0, 1, 2, 4, 0],
+];
+
+console.log(hourglassSum(matrix));
